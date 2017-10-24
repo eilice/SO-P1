f40e63c87747020e8c88af43abf29df83af4e6f4
eilice <alakolodziejska@gmail.com>
2017-10-24 18:25:49 +0200

    Add Makefile

--- main.c
+++ main.c
@@ -1,24 +1,24 @@
+// Alicja Kołodziejska
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-#include <unistd.h>
 
 void *thread_fun(void *ptr) {
-	//sleep(1);
 	printf("%s\n", (char *) ptr);
 	pthread_exit(NULL);
 }
 
 int main() {
-	const char *m1 = "Thread 1.";
-	const char *m2 = "Thread 2.";
+	const char *s1 = "Thread 1.", *s2 = "Thread 2.";
 	pthread_t t1, t2;
 	
-	pthread_create(&t1, NULL, thread_fun, (void*) m1));		
-	pthread_create(&t2, NULL, thread_fun, (void*) m2);
+	pthread_create(&t1, NULL, thread_fun, (void*) s1);
+	pthread_create(&t2, NULL, thread_fun, (void*) s2);
 	
 	pthread_join(t1, NULL);
 	pthread_join(t2, NULL);
+	
 	printf("Finished\n");
 	pthread_exit(NULL);
 	
